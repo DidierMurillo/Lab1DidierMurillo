69e4ed3bcb995faae67736bddbdb31c82ae194ec
Didier45 <didiermurillo@unitec.edu>
2016-07-29 17:08:52 -0400

    Ejercicio 1 casi terminado

--- Lab.cpp
+++ Lab.cpp
@@ -2,26 +2,11 @@
 #include <string>
 using namespace std;
  int Factorial(int);
+ void BuscandoNumero(int);
 
- int main (){	
-	int Total=0;
-	int Acumulador=0;
-	int Numero=0;
-	int Origin=0;
-	cout<<"Ingrese un numero:";
-	cin>>Numero;
-	Origin=Numero;
-	do{
-		Acumulador=Numero%10;
-	//	cout<<Acumulador<<"!+";
-		Numero=Numero/10;
-		Total=Total+Factorial(Acumulador);
-	}while(Numero>0);
-//	cout<<Total<<" Total \n" ;
-	if(Total==Origin){
-	cout<<"="<<Origin;
-	}else{
-	cout<<"La suma no da el mismo numero";
+ int main (){
+	for(int i=1;i<=50000;i++){
+	BuscandoNumero(i);
 	}
 	return 99;
  
@@ -35,9 +20,30 @@ using namespace std;
 	Acumulador=Acumulador*i;
 	
 	}
-	cout<<Acumulador<<"\n";
+//	cout<<Acumulador<<"\n";
 	return Acumulador;
  }	
+
+ void BuscandoNumero(int BNumero){
+	 int Total=0;
+        int Acumulador=0;
+        int Numero=BNumero;
+        int Origin=0;
+        Origin=Numero;
+        do{
+                Acumulador=Numero%10;
+        //      cout<<Acumulador<<"!+";
+                Numero=Numero/10;
+                Total=Total+Factorial(Acumulador);
+        }while(Numero>0);
+//      cout<<Total<<" Total \n" ;
+        if(Total==Origin){
+        cout<<"Resultado:"<<Total<<"\n";
+        }else{
+        
+        }
+}
+
 	  		
 	
 
