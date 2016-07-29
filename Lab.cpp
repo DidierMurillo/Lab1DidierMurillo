9580830cf70e4e051f91b92674d3ebd5caa8642d
Didier45 <didiermurillo@unitec.edu>
2016-07-29 18:28:38 -0400

    Lab Terminado

--- Lab.cpp
+++ Lab.cpp
@@ -11,28 +11,28 @@ using namespace std;
 	cout<<"Ejercicio 1:1"<<"\n"<<"Ejercicio 2:2"<<"\n"<<"Ingrese su opcion";
 	cin>>Opcion;
 	if(Opcion==1){
-	for(int i=1;i<=50000;i++){
-	BuscandoNumero(i);
-	}
+		for(int i=1;i<=50000;i++){
+			BuscandoNumero(i);
+		}
 	}if(Opcion==2){
-	int Numero1=0;
-	int Numero2=0;
-	int A=0,B=0;
-	cout<<"Ingrese el primer numero: ";
-	cin>>Numero1;
-	cout<<"\n";
-	cout<<"Ingrese el segundo numero:";
-	cin>>Numero2;
-//	cout<<"\n";
-	A=BuscarDivisores(Numero1);
-	B=BuscarDivisores(Numero2);
+		int Numero1=0;
+		int Numero2=0;
+		int A=0,B=0;
+		cout<<"Ingrese el primer numero: ";
+		cin>>Numero1;
+		cout<<"\n";
+		cout<<"Ingrese el segundo numero:";
+		cin>>Numero2;
+//		cout<<"\n";
+		A=BuscarDivisores(Numero1);
+		B=BuscarDivisores(Numero2);
 	if(A==Numero2&&B==Numero1){
-	cout<<"Los numeros son amigos";
+		cout<<"Los numeros son amigos";
 	}else{
-	cout<<"Los numeros no son amigos";
+		cout<<"Los numeros no son amigos";
 	}		
 	}else{
-	cout<<"Ingreso una opcion incorrecta";
+		cout<<"Ingreso una opcion incorrecta";
 
 	}
 	return 99;
@@ -45,7 +45,7 @@ using namespace std;
 	int Mismo=0;
 	Mismo=NumeroA; 
 	for (int  i=1;i<=NumeroA;i++){
-	Acumulador=Acumulador*i;
+		Acumulador=Acumulador*i;
 	
 	}
 //	cout<<Acumulador<<"\n";
@@ -53,7 +53,7 @@ using namespace std;
  }	
 
  void BuscandoNumero(int BNumero){
-	 int Total=0;
+	int Total=0;
         int Acumulador=0;
         int Numero=BNumero;
         int Origin=0;
@@ -64,9 +64,9 @@ using namespace std;
                 Numero=Numero/10;
                 Total=Total+Factorial(Acumulador);
         }while(Numero>0);
-//      cout<<Total<<" Total \n" ;
+//      	cout<<Total<<" Total \n" ;
         if(Total==Origin){
-        cout<<"Resultado:"<<Total<<"\n";
+       	 cout<<"Resultado:"<<Total<<"\n";
         }else{
         
         }
@@ -75,9 +75,9 @@ using namespace std;
  int BuscarDivisores(int NumeroX){
 	int SumaDivisores=0;
 	for(int i=1;i<NumeroX;i++ ){
-	if(NumeroX%i==0){
+		if(NumeroX%i==0){
 		SumaDivisores=SumaDivisores+i;
-	}
+		}
 	}
 	cout<<SumaDivisores<<"\n";
 	return SumaDivisores;
